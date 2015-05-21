ffea475ec558170b475ea4aa9426c8fac3dfdfe1
Rauckman <rauckmano@macbook-pro.ad.garmin.com>
2015-05-21 09:26:22 -0500

    changed ports to test

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+
+var server = require('http').createServer(app).listen(process.env.PORT || 8080);
+var io = require('socket.io').listen(server);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html')
