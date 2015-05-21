8a5f48c5cfaccf5067129ad87bce8bb7c26823f1
Rauckman <rauckmano@macbook-pro.ad.garmin.com>
2015-05-21 09:43:12 -0500

    removed code for test

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ var http = require('http').Server(app);
 var server = require('http').createServer(app).listen(process.env.PORT || 8080);
 var io = require('socket.io').listen(server);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html')
-});
+
 
 //connect and disconnect
 io.on('connection', function(socket){
